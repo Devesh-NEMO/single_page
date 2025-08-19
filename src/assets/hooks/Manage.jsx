75e961af993719b4296ec6b8a6664e4e61cb46ee
Devesh <nemopersonal.d@gmail.com>
2025-08-19 23:05:40 +0530

    --

--- src/assets/hooks/Manage.jsx
+++ src/assets/hooks/Manage.jsx
@@ -1,8 +1,14 @@
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
 
-const Manage = () => {
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+
+function App() {
   return (
-    <div>Manage</div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 }
-
-export default Manage
